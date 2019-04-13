65dd67f4dbad7df9ebdda7f3b7c4c022fc35b839
Ricky Cox <49468438+decimate@users.noreply.github.com>
2019-04-13 10:00:40 +0900

    Added file via upload

---
+++ SceneSwitch.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor.SceneManagement;
+
+// Create a cube to use as the trigger, and disable the Mesh Renderer.
+// Add this script to the trigger
+
+public class SceneSwitch : MonoBehaviour
+{
+
+	private void OnTriggerEnter(Collider other)
+	{
+		// Method 1 - Loads the next scene
+		EditorSceneManager.LoadScene(EditorSceneManager.GetActiveScene().buildIndex + 1);
+		
+		// Method 2 - Loads a specific scene, based on the Build index
+		//EditorSceneManager.LoadScene(1);
+	}
+
+}
